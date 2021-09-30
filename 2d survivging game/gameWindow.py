3cbe8e0feeeb8246bd5c422e9027bd4e056fd1c4
JeromeTheDome <91634830+JeromeTheDome@users.noreply.github.com>
2021-09-30 14:07:02 -0400

    Add files via upload

--- 2d survivging game/gameWindow.py
+++ 2d survivging game/gameWindow.py
@@ -3,8 +3,10 @@ import math
 
 pg = pygame
 
+programEnv = "replit"
+
 class ForeGround():
-  cursorIcon = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/background images/cursor.png")
+  cursorIcon = pg.image.load("./Images/background images/cursor.png")
 
   #inits the pygame window
   def __init__(self):
@@ -31,10 +33,10 @@ class Block():
     #all of these are defined at the top as many subclasses of block rely on them
 
     #defines air for default block type
-    air = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/air.png")
+    air = pg.image.load("./Images/block icons/air.png")
 
     #list variable for storing block data. structure is BlockMatrix[Horizontal Columns(uses y input)][Block in column(uses x input)][blockType]
-    BlockMatrix = [[[]for i in range(20)]for i in range(20)]
+    BlockMatrix = [[[]for i in range(26)]for i in range(26)]
 
     #sets every block to air
     for y in range(20):
@@ -47,11 +49,12 @@ class Block():
     #start of subclasses
     class Type():
 
-            air = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/air.png")
-            stone = None
-            dirt = None
-            grass = None
-            sand = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/sand.png")
+        air = pg.image.load("./Images/block icons/air.png")
+        stone = pg.image.load("./Images/block icons/sand.png")
+        dirt = pg.image.load("./Images/block icons/dirt.png")
+        grass = pg.image.load("./Images/block icons/grass.png")
+        sand = pg.image.load("./Images/block icons/sand.png")
+
 
 
     class Grid():
@@ -64,16 +67,26 @@ class Block():
 
         #set index of coordinates to 
             Block.BlockMatrix[y][x] = blockType
+        #shfits coordinate data for block matrix
+        def shiftBlockMatrix(rangeStartCoord,rangeEndCoord):
+          for y in range(0,25):
+            for x in range(rangeStartCoord,rangeEndCoord):
+              print("x:",x)
+              print("y:",y)
+              Block.BlockMatrix[y][x-1] = Block.BlockMatrix[y][x]
+              Block.BlockMatrix[y][x] = Block.Type.air
+      
+
 
 
 
     class Renderer():
         
-        air = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/air.png")
-        stone = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/sand.png")
-        dirt = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/sand.png")
-        grass = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/sand.png")
-        sand = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/block icons/sand.png")
+        air = pg.image.load("./Images/block icons/air.png")
+        stone = pg.image.load("./Images/block icons/sand.png")
+        dirt = pg.image.load("./Images/block icons/dirt.png")
+        grass = pg.image.load("./Images/block icons/grass.png")
+        sand = pg.image.load("./Images/block icons/sand.png")
 
         #takes coordinates from 0 to 20 on both axis
         def drawBlock(surface,blockType,position,skipCoordTranslation):
@@ -99,18 +112,23 @@ class Block():
 
 class Character():
     #things defined at top are used in many functions and subclasses
-    characterLocation = [400,440]
-    characterImage = pg.image.load("C:/Users/jerth/source/repos/GameSolution/2d survivging game/Images/Character Icons/John.png")
+    characterLocation = [13,19]
+    characterDrawLocation = [120,400]
+
+    characterImage = pg.image.load("./Images/Character Icons/john.png")
     class Move():
 
         #moves the character left or right
-        def Left(moveSpeed):
-            Character.characterLocation[0]-= moveSpeed + 2
-        def Right(moveSpeed):
-            Character.characterLocation[0]+=moveSpeed
+        def Left(moveAmount = 1):
+          Character.characterDrawLocation[0]-=moveAmount*40
+          Character.characterLocation[0]-=moveAmount
+
+        def Right(moveAmount = 1):
+            Character.characterLocation[0]+=moveAmount*40
+            Character.characterLocation[1]+=moveAmount
     class Render():
 
         def still(surface,imageIn):
         #renders the character still
-            surface.blit(imageIn,(Character.characterLocation[0],Character.characterLocation[1]))
+            surface.blit(imageIn,(Character.characterDrawLocation[0],Character.characterDrawLocation[1]))
          
--- 2d survivging game/main.py
+++ 2d survivging game/main.py
@@ -15,13 +15,13 @@ make sand edible
 pg = pygame
 
 #default values
+blockType = Block.Type.dirt
+
 #screen scroll values
 scrollSpeed = 0
 scrollDirection = 1
 #checks for iterations against a modulo operator. used for screens croll
 iterNum = 0
-#turns true for that frame if the player clicked on a square
-placeBlockAtEnd = False
 
 characterX = 10
 characterY = 10
@@ -30,12 +30,13 @@ characterY = 10
 ForeGround.__init__(1)
 
 #defines background image
-bgImage = BackGround.bgImage("/Users/jerth/source/repos/GameSolution/2d survivging game/Images/background images/cloud.png")
+bgImage = BackGround.bgImage("./Images/background images/cloud.png")
 
 #main game loop
 while True:
   #gets pygame events
   ev = pg.event.get()
+  keyboardInput = pg.key.get_pressed()
   #START
 
   """
@@ -59,23 +60,29 @@ while True:
 
 
 
+
   """
   block placement and properties logic
   """
   #grid matrix logic at top
 
+  #blockType logic
+  if keyboardInput[pg.K_1]:
+    blockType = Block.Type.grass
+  elif keyboardInput[pg.K_2]:
+    blockType = Block.Type.dirt
+  elif keyboardInput[pg.K_4]:
+    blockType = Block.Type.sand
 
-  #iterates through all events processed that frame and checks if event pg.MOUSEBUTTONDOWN occured
-
+  #checks mouse input
   if pg.mouse.get_pressed(3) == (True,False,False):
-    Block.Grid.placeBlock((ForeGround.getMousePos()[0],ForeGround.getMousePos()[1]),Block.Type.sand)
+    Block.Grid.placeBlock((ForeGround.getMousePos()[0],ForeGround.getMousePos()[1]),blockType)
   if pg.mouse.get_pressed(3) == (False,False,True):
     Block.Grid.placeBlock((ForeGround.getMousePos()[0],ForeGround.getMousePos()[1]),Block.Type.air)
 
   #for event in ev:
     #if event.type == pg.MOUSEBUTTONDOWN:
        # Block.Grid.placeBlock((ForeGround.getMousePos()[0],ForeGround.getMousePos()[1]),Block.Type.sand)
-
 
   #block rendering logic is to be placed at the bottom
 
@@ -101,13 +108,15 @@ while True:
   """
   player logic
   """
-  keyboardInput = pg.key.get_pressed()
+
+  #player movement Logic
   if keyboardInput[pg.K_a]:
-    Character.Move.Left(3)
+    Character.Move.Left()
+    #Block.Grid.shiftBlockMatrix()
 
   elif keyboardInput[pg.K_d]:
-    Character.Move.Right(5)
-
+    #Character.Move.Right()
+    Block.Grid.shiftBlockMatrix((Character.characterLocation[0])-10,(Character.characterLocation[0])+10)
     #draw character at the end AFTER(DO NOT FORGOR💀) setting game logic for position
   Character.Render.still(ForeGround.display,Character.characterImage)
 
